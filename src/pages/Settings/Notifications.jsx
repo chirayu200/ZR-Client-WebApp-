947e9e216e6abb68c59fb8bfacb93ce8eb83bb72
dhawal-1234 <129868829+dhawal-1234@users.noreply.github.com>
2023-09-07 21:31:08 +0530

    latest-changes

--- src/pages/Settings/Notifications.jsx
+++ src/pages/Settings/Notifications.jsx
@@ -17,75 +17,75 @@ export default function Notifications({setActive,franchiseeId}) {
     const [serviceConfirmation, setServiceConfirmation] = useState(false);
     const [connectionAlert, setConnectionAlert] = useState(false);
     const [membershipExpire, setMembershipExpire] = useState(false);
-    // const [dataMembership,setMembership] = useState({
-    //     location:locationId,
-    //     marketingCommunication:marketingCommunication,
-    //     serviceReminder:serviceRemainder,
-    //     connectionAlert:connectionAlert,
-    //     vaccination:vaccinationEnabled,
-    //     serviceConfirmation:serviceConfirmation,
-    //     membershipExpirationAlert:membershipExpire,
-    //     updatedBy:clientId,
-    //     status:2
-    // })
+ 
 
-    useEffect(() => {
-        getInitialData()
-    },[])
     const handleCheck = (name,event) => {
-        switch(name){
-            case 'MarketingCommunication':
-                setMarketingCommunication(event);
-                // setMembership(...dataMembership, ['MarketingCommunication']:event)
-                break;
-            
-            case 'Vaccination':
-                setVaccinationEnabled(event);
-                break;
-            case 'ServiceRemainder':
-                setServiceRemainder(event);
-                break;
-            case 'ServiceConfirmation':
-                setServiceConfirmation(event);
-                break;
-            case 'ConnectionAlert':
-                setConnectionAlert(event);
-                break;
-            case 'MembershipExpiration':
-                setMembershipExpire(event);
-                break;
-            default:
-                break;
-        }
-        // const dataMembershipString = JSON.stringify(dataMembership);
-        // console.log(dataMembershipString)
+        setNotification(name,event); 
     }
 
-    const getInitialData = () => {
-        getInitialSettings(clientId,locationId).then((response) =>{
-            console.log(response.data.Items[0].connectionAlert);
-            let value=response.data.Items[0].connectionAlert
-            setConnectionAlert(value)
+    const getInitialData = async () => {
+        await getInitialSettings(clientId,locationId).then((response) => {
+            console.log(response.data.Items[0]);  
+            if(response?.data?.Items[0])  {
+                console.log(response.data.Items[0]?.vaccination);
+                setMarketingCommunication(response.data.Items[0]?.marketingCommunication);
+                setVaccinationEnabled(response.data.Items[0]?.vaccination);
+                setServiceRemainder(response.data.Items[0]?.serviceReminder);
+                setServiceConfirmation(response.data.Items[0]?.serviceConfirmation);
+                setConnectionAlert(response.data.Items[0]?.connectionAlert);
+                setMembershipExpire(response.data.Items[0]?.membershipExpirationAlert);
+            } 
         })
     }
 
-    const setNotification = () =>{
-        const body = JSON.stringify({
-            location:locationId,
+    const setNotification = (name,event) => { 
+        const body = {
             marketingCommunication:marketingCommunication,
             serviceReminder:serviceRemainder,
             connectionAlert:connectionAlert,
             vaccination:vaccinationEnabled,
             serviceConfirmation:serviceConfirmation,
-            membershipExpirationAlert:membershipExpire,
-            updatedBy:clientId,
-            status:2
+            membershipExpirationAlert:membershipExpire,  
+            updatedBy: clientId,       
+            status:"1"
         }
-            )
 
-            updateSettings(clientId,franchiseeId,body)
-            setActive(0);
+        switch(name) {
+            case 'MarketingCommunication':
+                setMarketingCommunication(event);    
+                body.marketingCommunication = event;                             
+                break;            
+            case 'Vaccination':
+                setVaccinationEnabled(event);                
+                body.vaccination = event;
+                break;
+            case 'ServiceRemainder':
+                setServiceRemainder(event);  
+                body.serviceReminder = event;              
+                break;
+            case 'ServiceConfirmation':
+                setServiceConfirmation(event);      
+                body.serviceConfirmation = event;          
+                break;
+            case 'ConnectionAlert':
+                setConnectionAlert(event);          
+                body.connectionAlert = event;      
+                break;
+            case 'MembershipExpiration':
+                setMembershipExpire(event);         
+                body.membershipExpirationAlert = event;       
+                break;
+            default:
+                break;
+        }
+         updateSettings(clientId,locationId,body)            
     }
+
+    useEffect(() => {
+        getInitialData();
+    },[]);
+
+
     return (
         <>
             <Container className='appointment-container'>
@@ -97,7 +97,7 @@ export default function Notifications({setActive,franchiseeId}) {
                                 color='#E35205'
                                 icon={backArrow}
                                 backgroundColor='#E7EFF9'
-                                onClick={() => setNotification()}
+                                onClick={() =>setActive(0)}
                             />
                             <Typography className='header-text-blue font-weight-700 f-18'>Notifications</Typography>
                         </Box>
@@ -108,21 +108,21 @@ export default function Notifications({setActive,franchiseeId}) {
                 <Grid container>
                     <Grid item xs={5.5} md={5.5} sm={5.5} className="display-flex">
                         <Typography className="header-text-black font-weight-700">Marketing Communication</Typography>
-                        <Toggle onChange={(e) => handleCheck("MarketingCommunication",e)} checked={!!marketingCommunication}/>
+                        <Toggle onChange={(e) => handleCheck("MarketingCommunication",e)} value={marketingCommunication} />
                     </Grid>
                     <Grid item xs={1} md={1} sm={1}/>
                     <Grid item xs={5.5} md={5.5} sm={5.5}  className="display-flex">
                         <Typography className="header-text-black font-weight-700">Vaccination</Typography>
-                        <Toggle onChange={(e) => handleCheck("Vaccination",e)}  checked={vaccinationEnabled}/>
+                        <Toggle onChange={(e) => handleCheck("Vaccination",e)} value={vaccinationEnabled} />
                     </Grid>
                     <Grid item xs={5.5} md={5.5} sm={5.5}  className="display-flex" sx={{mt:3}}>
                         <Typography className="header-text-black font-weight-700">Service Reminder</Typography>
-                        <Toggle onChange={(e) => handleCheck("ServiceRemainder",e)}  checked={serviceRemainder}/>
+                        <Toggle onChange={(e) => handleCheck("ServiceRemainder",e)} value={serviceRemainder} />
                     </Grid>
                     <Grid item xs={1} md={1} sm={1}/>
                     <Grid item xs={5.5} md={5.5} sm={5.5}  className="display-flex" sx={{mt:3}}>
                         <Typography className="header-text-black font-weight-700">Service Confirmation</Typography>
-                        <Toggle onChange={(e) => handleCheck("ServiceConfirmation",e)}  checked={serviceConfirmation}/>
+                        <Toggle onChange={(e) => handleCheck("ServiceConfirmation",e)} value={serviceConfirmation}  />
                     </Grid>
                     <Grid item xs={5.5} md={5.5} sm={5.5}  className="display-flex" sx={{mt:3}}>
                         <Typography className="header-text-black font-weight-700">Connection Alert</Typography>
@@ -131,7 +131,7 @@ export default function Notifications({setActive,franchiseeId}) {
                     <Grid item xs={1} md={1} sm={1}/>
                     <Grid item xs={5.5} md={5.5} sm={5.5}  className="display-flex" sx={{mt:3}}>
                         <Typography className="header-text-black font-weight-700">Membership Expiration Alert</Typography>
-                        <Toggle onChange={(e) => handleCheck("MembershipExpiration",e)}  checked={membershipExpire}/>
+                        <Toggle onChange={(e) => handleCheck("MembershipExpiration",e)}  value={membershipExpire} />
                     </Grid>
                 </Grid>
             </Container>
